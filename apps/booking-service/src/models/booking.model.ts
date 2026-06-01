70cf56c1df94309950e7d722cfb20187d18f6c93
Aryan <aryanlytics@gmail.com>
2026-06-01 19:53:28 +0500

    refactor: clean up typo in geospatial index comment within booking model

--- apps/booking-service/src/models/booking.model.ts
+++ apps/booking-service/src/models/booking.model.ts
@@ -48,7 +48,7 @@ const bookingSchema = new Schema<IBookingDocument>(
   }
 );
 
-// Geospatial in
+// Geospatial 
 bookingSchema.index({ location: "2dsphere" });
 
 export const Booking = mongoose.model<IBookingDocument>("Booking", bookingSchema);
