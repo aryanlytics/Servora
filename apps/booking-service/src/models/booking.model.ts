ac898b6e5394cb418e4f1f06a3398f6d9246b021
Aryan <aryanlytics@gmail.com>
2026-06-01 19:52:25 +0500

    refactor: update geospatial index comment in booking model

--- apps/booking-service/src/models/booking.model.ts
+++ apps/booking-service/src/models/booking.model.ts
@@ -48,7 +48,7 @@ const bookingSchema = new Schema<IBookingDocument>(
   }
 );
 
-// Geospatial index on bok
+// Geospatial index on bk
 bookingSchema.index({ location: "2dsphere" });
 
 export const Booking = mongoose.model<IBookingDocument>("Booking", bookingSchema);
